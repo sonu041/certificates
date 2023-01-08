e6ed479957da3b8c17bc46f7a1cb03f1c95c9fc8
Shuvankar Sarkar <sonu041@gmail.com>
2023-01-08 23:39:27 +0530

    Added GCP cert description

--- index.php
+++ index.php
@@ -26,20 +26,36 @@
       <h1>Certificates</h1>
     </div>
   </header>
-
-  <section class="timeline">
-    <div class="container">
-    <?php
-    // Read the JSON file 
-    $json = file_get_contents('https://raw.githubusercontent.com/sonu041/certificates/main/certificates.json');
+  <?php
+  // Read the JSON file
+  $json = file_get_contents('https://raw.githubusercontent.com/sonu041/certificates/main/certificates.json');
       
-    // Decode the JSON file
-    $json_data = json_decode($json,true);
+  // Decode the JSON file
+  $json_data = json_decode($json,true);
 
+  if(isset($_GET['view']) && $_GET['view'] == 'list') {
+  ?>
+  <section>
+  <?php
     foreach ($json_data as $key => $value) {
       foreach ($value as $key => $val) {
     ?>
-    
+    <p><?php echo $val['date'].' - '.$val['description']; ?></p>
+    <?php
+      }
+    }
+    ?>
+  </section>
+  
+  <?php
+  } else {
+  ?>
+  <section class="timeline">
+    <div class="container">
+    <?php
+    foreach ($json_data as $key => $value) {
+      foreach ($value as $key => $val) {
+    ?>
         <div class="timeline-item">
           <div class="timeline-img"></div>
           <div class="timeline-content timeline-card <?php echo $key%2==0?'js--fadeInLeft':'js--fadeInRight'; ?>">
@@ -51,6 +67,7 @@
     <?php
       }
     }
+  }
     ?>
     </div>
   </section>   
